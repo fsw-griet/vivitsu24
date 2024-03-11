6aa590f01e3a2e05d3ae0eea740a0b31f318cf99
kvenkatamar <78538730+kvenkatamar@users.noreply.github.com>
2024-03-11 19:16:48 +0530

    faqs

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,8 +5,8 @@ let image4 = document.getElementById("img4");
 
 let images1 = ["IMG_3018.JPG", "IMG_3041.JPG", "IMG_3053.JPG"];
 let images2 = ["IMG_3101.JPG", "IMG_3154.JPG", "IMG_3207.JPG"];
-let images4 = ["IMG_3398.JPG", "IMG_3431.JPG", "IMG_3435.JPG"];
-let images3 = ["IMG_3436.JPG", "IMG_3443.JPG", "IMG_3468.JPG"];
+let images4 = ["IMG_3398.JPG", "IMG_3431.JPG", "IMG_3468.JPG"];
+let images3 = ["IMG_3436.JPG", "IMG_3443.JPG", "IMG_3435.JPG"];
 
 setInterval(function () {
   let ran = Math.floor(Math.random() * images1.length);
